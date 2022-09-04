2a1b6dc2ffe8bee4fd8ead9b0e5ee17cacf58bb9
Константин <eckumoc@gmail.com>
2022-09-04 06:17:31 +0300

    ╨б╨╛╨╖╨┤╨░╤О ╨╜╨╛╨▓╤Л╨╣ ╨┐╤А╨╛╨╡╨║╤В

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_TestApplication
+{
+    internal class TestApplicationProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TestApplicationModule/TestApplicationProgram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+public class TestApplicationProgram
+{
+    public void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
